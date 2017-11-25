62dc568af6647b5a143eeae57167e100de68c458
Vini85428026 <marciomarciovw@gmail.com>
2017-11-25 12:18:31 -0300

    recebendo questionario

--- src/br/com/fametro/dsw/servlets/Questionario.java
+++ src/br/com/fametro/dsw/servlets/Questionario.java
@@ -39,6 +39,15 @@ public class Questionario extends HttpServlet {
 		String radio2 = request.getParameter("radios2");
 		String radio3 = request.getParameter("radios3");
 		String radio4 = request.getParameter("radios4");
+		String radio5 = request.getParameter("radios5");
+		String radio6 = request.getParameter("radios6");
+		String radio7 = request.getParameter("radios7");
+		String radio8 = request.getParameter("radios8");
+		String radio9 = request.getParameter("radios9");
+		String radio10 = request.getParameter("radios10");
+		String radio11 = request.getParameter("radios11");
+		String radio12 = request.getParameter("radios12");
+		String radio13 = request.getParameter("radios13");
 		
 		System.out.println(radio1 + radio2 + radio3 + radio4);
 				
